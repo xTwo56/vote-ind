a1b5aaf2129868fe0baa2f7478b838874c0aff0f
xTwo56 <153709773+xTwo56@users.noreply.github.com>
2024-06-20 16:56:39 +0530

    contract evm, enum v0 init

---
+++ contracts/enum.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+enum MaritalStatus {
+  Single, 
+  Married, 
+  Divorced,
+  MultipleMarried,
+  PartnerDead
+}
+
+enum Party {
+  AAP,
+  BJP, 
+  Congress
+}
+
+enum Time {
+  Years,
+  Days,
+  Hours,
+  Minutes,
+  Seconds
+}
+
+enum Sex {
+  Male, 
+  Female,
+  Non-Binary,
+  ChowmeinWithChilliPotato
+}
---
+++ contracts/evm.sol
@@ -0,0 +1,120 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./enums.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract EVM is Initializable {
+
+  address owner ;
+  address admin ;
+
+  uint votingStartTime ;
+  uint votingEndTime ;
+  bool electionStatus = false;
+
+  struct Profile {
+    string name ;
+    uint8 age ;
+    Sex sex ;
+    MaritalStatus status;
+  }
+
+  struct Voter {
+    Profile person;
+    address voterAddress;
+    bool voted ;
+    string aadharId ;
+  }
+  
+  struct VotingProviders {
+    string name;
+    uint number;
+    bool isVerified;
+  }
+
+  mapping (address => Voter) individualVoters;
+  mapping (address => Voter[]) votingProvidersMap;
+  mapping (address => VotingProviders) votingProviders;
+  mapping (string => Party) voterToParty;
+  mapping (Party => uint) totalVotes;
+
+  modifier onlyOwner(){
+    require(msg.sender == owner, "onlyOwnerCallable");
+    _;
+  }
+
+  modifier onlyAdmin(){
+    require(msg.sender == admin || msg.sender == owner, "onlyAdminCallable");
+    _;
+  }
+
+  function initialize() public initializer {
+    admin = owner = msg.sender;
+
+  } 
+
+  function setAdmin(address _admin) external {
+    admin = _admin;
+  }
+  
+  function startVoting(Time unit, uint magnitude) external onlyAdmin {
+    require(electionStatus == false, "Election already active");
+
+    votingStartTime = block.timestamp;
+    if(unit == Time.Days) votingEndTime = block.timestamp + magnitude*1 days;
+    else if(unit == Time.Weeks) votingEndTime = block.timestamp + magnitude*1 weeks;
+    else if(unit == Time.Hours) votingEndTime = block.timestamp + magnitude*1 hours;
+    else if(unit == Time.Minutes) votingEndTime = block.timestamp + magnitude*1 minutes;
+    else if(unit == Time.Seconds) votingEndTime = block.timestamp + magnitude*1 seconds;
+    
+    electionStatus = true;
+  }
+
+  function endVoting() external onlyAdmin {
+    electionStatus = false;
+  }
+
+  function isVerified(Voter memory voter) public pure returns(bool){
+    return bytes(voter.aadharId).length!=0;
+  }
+
+  function vote(Voter memory voter, Party _party) public {
+       voterToParty[voter.aadharId];
+       totalVotes[_party]++;
+  }
+
+  function voteByIndividual(Party _party) public {
+    Voter memory voter = individualVoters[msg.sender];
+    require(isVerified(voter), "voter not verified");
+    require(voterToParty[voter.aadharId] == Party(0), "already voted");
+
+    vote(voter, _party);
+  }
+
+  function voteByProviders(uint voterId, Party _party)external {
+    Voter memory voter = votingProvidersMap[msg.sender][voterId];
+    require(isVerified(voter), "voter not verified");
+    require(voterToParty[voter.aadharId] == Party(0), "already voted");
+
+    vote(voter, _party);
+    voter.voted = true;
+  }
+  
+  function addVoter(Profile memory _profile, address voterAddress, bool _voted, string memory _aadharId)public returns(uint){
+    Voter memory voter = Voter(_profile, voterAddress, _voted, _aadharId);
+    if(!votingProviders[msg.sender].isVerified){
+      individualVoters[msg.sender] = voter;
+      return 0;
+    }else{
+      votingProvidersMap[msg.sender].push(voter);
+      return votingProvidersMap[msg.sender].length -1;
+    }
+  }
+  
+  function getTotalVotes()public view returns(uint){
+    return totalVotes[Party.AAP]+totalVotes[Party.BJP]+totalVotes[Party.Congress];
+  }
+}
+
+
